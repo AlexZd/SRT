30947f7c08f88a202d91d38777ca2bd4822371e4
Alex Zdorovets <alexander@farabi.ae>
2016-01-31 10:14:20 +0400

    Router added

---
+++ Pod/Utils/Router.swift
@@ -0,0 +1,50 @@
+//
+//  Router.swift
+//
+//  Created by Alex Zdorovets on 6/17/15.
+//  Copyright (c) 2015 Alex Zdorovets. All rights reserved.
+//
+
+import UIKit
+
+class Router: NSObject {
+    static let FTFlipAnimationDuration = 0.5
+    static let sharedInstance = FTRouter()
+    
+    var window : UIWindow
+    
+    //MARK: - FTRouter lifecycle
+    
+    override init() {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        super.init()
+        self.showInitialViewController()
+        self.window.makeKeyAndVisible()
+    }
+    
+    //MARK: - Routing
+    
+    func showInitialViewController() {
+        let viewController = UIStoryboard.mainStoryboard.instantiateInitialViewController()!
+        self.setWindowRootViewController(viewController, completion: nil)
+    }
+    
+    //MARK: - Utils
+    
+    func setWindowRootViewController(viewController:UIViewController, completion: ((finished : Bool) -> Void)?) {
+        viewController.view.frame = self.window.frame
+        viewController.view.updateConstraints()
+        let oldState = UIView.areAnimationsEnabled()
+        if self.window.rootViewController == nil {
+            UIView.setAnimationsEnabled(oldState)
+            self.window.rootViewController = viewController
+            completion?(finished: true)
+        }else{
+            UIView.transitionFromView(self.window.rootViewController!.view!, toView: viewController.view, duration: FTRouter.FTFlipAnimationDuration, options: UIViewAnimationOptions.TransitionFlipFromLeft) { [unowned self] (finished) -> Void in
+                UIView.setAnimationsEnabled(oldState)
+                self.window.rootViewController = viewController
+                completion?(finished: finished)
+            }
+        }
+    }
+}
