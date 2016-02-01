3ad6b412e705148bc041f87a0e6f26975efc7a3f
Alex Zdorovets <alexander@farabi.ae>
2016-02-01 12:20:43 +0400

    PHPhotoLibrary fix

--- Pod/Utils/PHPhotoLibrary+Helpers.swift
+++ Pod/Utils/PHPhotoLibrary+Helpers.swift
@@ -12,12 +12,14 @@ extension PHPhotoLibrary {
     /** Checks and asks (if needed) Photo library permissions  */
     class func isAuthorized(completion:(authorized:Bool) -> Void) {
         PHPhotoLibrary.requestAuthorization { status in
-            switch status {
-            case .Authorized:
-                completion(authorized: true)
-            default:
-                completion(authorized: false)
-            }
+            dispatch_async(dispatch_get_main_queue(),{
+                switch status {
+                case .Authorized:
+                    completion(authorized: true)
+                default:
+                    completion(authorized: false)
+                }
+            })
         }
     }
 }
