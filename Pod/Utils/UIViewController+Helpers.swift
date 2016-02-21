59ce530377cde086b223c96c9217ed8aa0fd7b90
Alex Zdorovets <alexander@farabi.ae>
2016-02-21 12:20:31 +0400

    fixIOS9PopOverAnchor

--- Pod/Utils/UIViewController+Helpers.swift
+++ Pod/Utils/UIViewController+Helpers.swift
@@ -1,5 +1,5 @@
 //
-//  UIViewController+Error.swift
+//  UIViewController+Helpers.swift
 //
 //  Created by Alex Zdorovets on 6/18/15.
 //  Copyright (c) 2015 Alex Zdorovets. All rights reserved.
@@ -14,5 +14,14 @@ extension UIViewController {
     
     public func showError(title:String, error:NSError) {
         UIAlertView(title: title as String, message: error.localizedDescription, delegate: self, cancelButtonTitle: "Ok").show()
+    }
+    
+    public func fixIOS9PopOverAnchor(segue:UIStoryboardSegue?) {
+        guard #available(iOS 9.0, *) else {
+            return
+        }
+        if let popOver = segue?.destinationViewController.popoverPresentationController, let anchor  = popOver.sourceView where popOver.sourceRect == CGRect() && segue!.sourceViewController === self {
+            popOver.sourceRect = anchor.bounds
+        }
     }
 }
