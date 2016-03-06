0e9a5a3d3d643afab7f2632455532e9c421024bc
AlexZd <alexzd89@mail.ru>
2016-03-06 14:34:53 +0400

    Model interaction

--- Pod/Utils/ModelInteraction.swift
+++ Pod/Utils/ModelInteraction.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+protocol ModelInteraction: class {
+    func shouldAdd(model: AnyObject, options: AnyObject?) -> Bool
+    func shouldDelete(model: AnyObject, options: AnyObject?) -> Bool
+    func shouldUpdate(model: AnyObject, options: AnyObject?) -> Bool
+    
+    func willAdd(model: AnyObject, options: AnyObject?)
+    func willUpdate(model: AnyObject, options: AnyObject?)
+    func willDelete(model: AnyObject, options: AnyObject?)
+    
+    func didAdd(model: AnyObject, options: AnyObject?)
+    func didUpdate(model: AnyObject, options: AnyObject?)
+    func didDelete(model: AnyObject, options: AnyObject?)
+}
+
+extension ModelInteraction {
+    //MARK: - Shoulda
+    
+    func shouldAdd(model: AnyObject, options: AnyObject? = [:]) -> Bool {
+        return true
+    }
+    
+    func shouldDelete(model: AnyObject, options: AnyObject? = [:]) -> Bool {
+        return true
+    }
+    
+    func shouldUpdate(model: AnyObject, options: AnyObject? = [:]) -> Bool {
+        return true
+    }
+    
+    //MARK: - Will
+    
+    func willAdd(model: AnyObject, options: AnyObject? = [:]) {}
+    func willUpdate(model: AnyObject, options: AnyObject? = [:]) {}
+    func willDelete(model: AnyObject, options: AnyObject? = [:]) {}
+    
+    //MARK: - Did
+    
+    func didAdd(model: AnyObject, options: AnyObject? = [:]) {}
+    func didUpdate(model: AnyObject, options: AnyObject? = [:]) {}
+    func didDelete(model: AnyObject, options: AnyObject? = [:]) {}
+}
