373c3c5475f252c4f755fe604444b2127b3217ab
AlexZd <alexzd89@mail.ru>
2016-03-20 17:15:50 +0400

    Dictionary methods marked as public

--- Pod/Utils/Dictionary+Helpers.swift
+++ Pod/Utils/Dictionary+Helpers.swift
@@ -7,13 +7,13 @@
 
 import Foundation
 
-func += <KeyType, ValueType> (inout left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
+public func += <KeyType, ValueType> (inout left: Dictionary<KeyType, ValueType>, right: Dictionary<KeyType, ValueType>) {
     for (k, v) in right {
         left.updateValue(v, forKey: k)
     }
 }
 
-func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>) -> Dictionary<K,V> {
+public func + <K,V>(left: Dictionary<K,V>, right: Dictionary<K,V>) -> Dictionary<K,V> {
     var map = Dictionary<K,V>()
     for (k, v) in left {
         map[k] = v
