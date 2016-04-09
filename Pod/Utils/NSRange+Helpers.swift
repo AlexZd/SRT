d2f346829f731602886ee184e29646da1ca38aec
Alex <Alex@MacBook-Pro-Alex.local>
2016-04-09 19:22:21 +0400

    NSRange zero method

---
+++ Pod/Utils/NSRange+Helpers.swift
@@ -0,0 +1,17 @@
+//
+//  NSRange+Helpers.swift
+//  Pods
+//
+//  Created by Alex on 09.04.16.
+//
+//
+
+import Foundation
+
+extension NSRange {
+    
+    public static var zero: NSRange {
+        return NSRange(location: 0, length: 0)
+    }
+    
+}
