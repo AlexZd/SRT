80d462332528213d2b70eefde4eb5e181799dd32
AlexZd <alexzd89@mail.ru>
2016-04-13 16:52:44 +0400

    NSFileManager methods moved from NSObject

--- Pod/Utils/NSFileManager+Helpers.swift
+++ Pod/Utils/NSFileManager+Helpers.swift
@@ -16,4 +16,54 @@ extension NSFileManager {
         public static let Temp = NSTemporaryDirectory()
     }
     
+    /** Print list of Documents files */
+    public class func listFiles(folder:String?) {
+        let paths = NSFileManager.Directory.Documents
+        
+        guard let path = (folder != nil ? NSURL(string: paths)?.URLByAppendingPathComponent(folder!).absoluteString : paths) else {
+            return
+        }
+        
+        do {
+            let directoryContent = try NSFileManager.defaultManager().contentsOfDirectoryAtPath(path)
+            print("+--------------------------------------+")
+            print("| FILES:                               |")
+            print("+--------------------------------------+")
+            for i in 0..<directoryContent.count {
+                print("| \(i + 1). \(directoryContent[i])")
+            }
+            print("+--------------------------------------+")
+        } catch let error as NSError {
+            print("Could not find path: \(error)")
+        }
+    }
+    
+    /** Remove file with name from Documents folder */
+    public class func removeFile(fileName:String) {
+        guard let fileURL = NSURL(string: NSFileManager.Directory.Documents)?.URLByAppendingPathComponent(fileName) else {
+            print("File not found")
+            return
+        }
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(fileURL)
+            print("File \(fileName) removed")
+        } catch let error as NSError {
+            print("Could not delete file: \(error)")
+        }
+    }
+    
+    /** Remove file at path */
+    public class func removeFileAt(path:String?) {
+        guard let path = path else {
+            return
+        }
+        let fileURL = NSURL(fileURLWithPath: path)
+        do {
+            try NSFileManager.defaultManager().removeItemAtURL(fileURL)
+            print("File at \(path) removed")
+        } catch let error as NSError {
+            print("Could not delete file: \(error)")
+        }
+    }
+    
 }
--- Pod/Utils/NSObject+Helpers.swift
+++ Pod/Utils/NSObject+Helpers.swift
@@ -10,57 +10,7 @@ import Foundation
 extension NSObject {
     /** Returns class description. Useful for identifiers.  */
     public class var identifier : String {
-        get {
-            return NSStringFromClass(self).componentsSeparatedByString(".").last!
-        }
-    }
-    
-    /** Print list of Documents files */
-    public func listFiles(folder:String?) {
-        let paths = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory, NSSearchPathDomainMask.UserDomainMask, true)
-
-        guard let path = folder != nil ? NSURL(string: paths.first!)?.URLByAppendingPathComponent(folder!).absoluteString : paths.first else {
-            return
-        }
-        
-        do {
-            let directoryContent = try NSFileManager.defaultManager().contentsOfDirectoryAtPath(path)
-            print("+--------------------------------------+")
-            print("| FILES:                               |")
-            print("+--------------------------------------+")
-            for i in 0..<directoryContent.count {
-                print("| \(i + 1). \(directoryContent[i])")
-            }
-            print("+--------------------------------------+")
-        } catch let error as NSError {
-            print("Could not delete file: \(error)")
-        }
-
-    }
-    
-    /** Remove file with name from Documents folder */
-    public func removeFile(fileName:String) {
-        guard let fileURL = NSURL(string: NSFileManager.Directory.Documents)?.URLByAppendingPathComponent(fileName) else {
-            print("File not found")
-            return
-        }
-        do {
-            try NSFileManager.defaultManager().removeItemAtURL(fileURL)
-            print("File \(fileName) removed")
-        } catch let error as NSError {
-            print("Could not delete file: \(error)")
-        }
-    }
-    
-    /** Remove file at path */
-    public func removeFileAt(path:String) {
-        let fileURL = NSURL(fileURLWithPath: path)
-        do {
-            try NSFileManager.defaultManager().removeItemAtURL(fileURL)
-            print("File at \(path) removed")
-        } catch let error as NSError {
-            print("Could not delete file: \(error)")
-        }
+        return NSStringFromClass(self).componentsSeparatedByString(".").last!
     }
     
     /** Do something after delay */
