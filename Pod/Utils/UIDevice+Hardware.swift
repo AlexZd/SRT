ed0283347b3739b86b10e025ba75306a1d7215b4
AlexZd <alexzd89@mail.ru>
2016-05-18 12:07:43 +0400

    Ipad pro check

--- Pod/Utils/UIDevice+Hardware.swift
+++ Pod/Utils/UIDevice+Hardware.swift
@@ -8,10 +8,14 @@
 import UIKit
 
 public let IS_IPAD : Bool = UIDevice.currentDevice().userInterfaceIdiom == .Pad
-public let IS_IPOD : Bool = UIDevice.currentDevice().model.rangeOfString("iPod") != nil
+public let IS_IPAD_PRO_BIG : Bool = (IS_IPAD && UIScreen.mainScreen().bounds.size.height == 1366.0)
+
 public let IS_IPHONE : Bool = UIDevice.currentDevice().userInterfaceIdiom == .Phone
 public let IS_IPHONE_4 : Bool = (IS_IPHONE && UIScreen.mainScreen().bounds.size.height == 480.0)
 public let IS_IPHONE_5 : Bool = (IS_IPHONE && UIScreen.mainScreen().bounds.size.height == 568.0)
 public let IS_IPHONE_6 : Bool = (IS_IPHONE && UIScreen.mainScreen().bounds.size.height == 667.0)
 public let IS_IPHONE_6_PLUS : Bool = (IS_IPHONE && UIScreen.mainScreen().bounds.size.height == 736.0)
+
+public let IS_IPOD : Bool = UIDevice.currentDevice().model.rangeOfString("iPod") != nil
+
 public let IS_RETINA : Bool = UIScreen.mainScreen().scale == 2
