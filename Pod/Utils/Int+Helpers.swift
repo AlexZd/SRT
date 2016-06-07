706a92bbe916ff6b18ec343987b9f4d3ea42fb81
AlexZd <alexzd89@mail.ru>
2016-06-07 11:29:36 +0400

    Int helpers

--- Pod/Utils/Int+Helpers.swift
+++ Pod/Utils/Int+Helpers.swift
@@ -10,11 +10,19 @@ import Foundation
 
 extension Int {
     
+    public var isEven: Bool { return self % 2 == 0 }
+    public var isOdd: Bool { return self % 2 != 0 }
+    public var isPositive: Bool { return self > 0 }
+    public var isNegative: Bool { return self < 0 }
+    public var range: Range<Int> { return 0..<self }
+    
+    /** Returns thousand separated representation of number */
     public func thousandSeparated(separator:String?) -> String {
         return NSNumber(integer: self).thousandSeparated(separator)
     }
     
-    public func roundUpToInteger (divisor: Int) -> Int {
+    
+    public func roundUpToInteger(divisor: Int) -> Int {
         let rem = self % divisor
         return rem == 0 ? self : self + divisor - rem
     }
