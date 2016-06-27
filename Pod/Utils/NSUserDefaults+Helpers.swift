b6e60850fff74b1eaba82e0a5fe34f8c59840a14
AlexZd <alexzd89@mail.ru>
2016-06-27 11:35:00 +0400

    NSUserDefaults helpers

--- Pod/Utils/NSUserDefaults+Helpers.swift
+++ Pod/Utils/NSUserDefaults+Helpers.swift
@@ -1,6 +1,5 @@
 //
 //  NSUserDefaults+Helpers.swift
-//  Pods
 //
 //  Created by Alex Zdorovets on 5/18/16.
 //
@@ -10,7 +9,23 @@ import Foundation
 
 extension NSUserDefaults {
     
-    public func setValue(value: AnyObject?, forKey key: String, synchronize: Bool) {
+    //MARK: - Class
+    
+    public class func get<T>(key:String) -> T? {
+        return NSUserDefaults.standardUserDefaults().get(key)
+    }
+    
+    public class func set(value:AnyObject?, key:String, synchronize:Bool = true) {
+        NSUserDefaults.standardUserDefaults().set(value, key: key, synchronize: synchronize)
+    }
+    
+    //MARK: - Instance
+    
+    public func get<T>(key:String) -> T? {
+        return NSUserDefaults.standardUserDefaults().valueForKey(key) as? T
+    }
+    
+    public func set(value: AnyObject?, key: String, synchronize: Bool = true) {
         self.setValue(value, forKey: key)
         if synchronize {
             self.synchronize()
