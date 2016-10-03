fcc63f7c3186e9673aebb6e6058461053115b9d4
AlexZd <alexzd89@mail.ru>
2016-10-03 15:00:20 +0400

    show error updated

--- Pod/Utils/AlertRepresentable.swift
+++ Pod/Utils/AlertRepresentable.swift
@@ -9,29 +9,22 @@
 import Foundation
 
 public protocol AlertRepresentable {
-    func present(error:NSError)
-    func present(title:String, error:NSError)
-    func present(title:String, text:String?)
+    func show(error:Error)
+    func show(title:String, error:Error)
+    func show(title:String, text:String?)
 }
 
 extension AlertRepresentable where Self: UIViewController {
     
-    public func present(error:NSError) {
+    public func show(error:Error) {
         self.present(title: "Error", error: error)
     }
     
-    public func present(title:String, error:NSError) {
-        let CRUDResponseDataKey = "CRUDResponseDataKey"
-        if let errorsInfo = error.userInfo[CRUDResponseDataKey] as? [String] , errorsInfo.isEmpty == false {
-            self.present(title: title, text: errorsInfo.first)
-        } else if let errorsInfo = error.userInfo[CRUDResponseDataKey] as? [String: String] , errorsInfo.isEmpty == false {
-            self.present(title: title, text: errorsInfo["error"])
-        } else {
-            self.present(title: title, text: error.localizedDescription)
-        }
+    public func show(title:String, error:Error) {
+        self.present(title: title, text: error.localizedDescription)
     }
     
-    public func present(title:String, text:String?) {
+    public func show(title:String, text:String?) {
         let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("ok", comment: "").up, style: .cancel, handler: nil))
         self.present(alert, animated: true, completion: nil)
