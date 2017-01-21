5705cbc3ee1718d8faee0edca09157e9eb168acb
Alex <Alex@MacBook-Pro-Alex.local>
2017-01-21 17:44:42 +0400

    add unique

--- Pod/Utils/Array+Helpers.swift
+++ Pod/Utils/Array+Helpers.swift
@@ -7,6 +7,19 @@
 //
 
 
-extension Array {
-    
+extension Array where Element: Equatable {
+    /** Remove first collection element that is equal to the given 'object' */
+    public mutating func remove(object: Element?) {
+        guard let item = object else { return }
+        if let index = self.index(of: item) {
+            self.remove(at: index)
+        }
+    }
+}
+
+extension Array where Element: Hashable {
+    /** Returns unique values of Array */
+    public var unique: [Element] {
+        return Array(Set(self))
+    }
 }
