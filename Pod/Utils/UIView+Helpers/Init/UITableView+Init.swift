1f93ae19ac6bd4ce38ae840554731442d4758489
mojidabckuu <mojidabckuu.22.06.92@gmail.com>
2018-01-25 02:04:58 +0300

    Add supp UIView inits

---
+++ Pod/Utils/UIView+Helpers/Init/UITableView+Init.swift
@@ -0,0 +1,33 @@
+//
+//  UITableView+Helpers.swift
+//  Scene
+//
+//  Created by Alex on 19.05.17.
+//  Copyright © 2017 AlexZd. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UITableView {
+    
+    public static func table(separatorInset: UIEdgeInsets = .zero, separatorColor: UIColor? = nil, multiply: Bool = false) -> UITableView {
+        let tableView = UITableView(frame: .zero, style: .plain)
+        tableView.prepareTable(separatorInset: separatorInset, separatorColor: separatorColor, multiply: multiply)
+        return tableView
+    }
+    
+    func prepareTable(separatorInset: UIEdgeInsets = .zero, separatorColor: UIColor? = nil, multiply: Bool = false) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        self.allowsMultipleSelection = multiply
+        self.separatorInset = separatorInset
+        self.tableFooterView = UIView()
+        self.showsVerticalScrollIndicator = false
+        self.estimatedSectionFooterHeight = 0
+        self.estimatedSectionHeaderHeight = 0
+        if let color = separatorColor {
+            self.separatorColor = color
+        }
+    }
+    
+}
