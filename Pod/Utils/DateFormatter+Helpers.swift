9f4faa3ed03ed099be53c3fa18dcec0dfe9c2dab
Alex <alexzd89@mail.ru>
2019-03-23 17:25:05 +0400

    UTCDateFormatter added

--- Pod/Utils/DateFormatter+Helpers.swift
+++ Pod/Utils/DateFormatter+Helpers.swift
@@ -25,6 +25,16 @@ extension DateFormatter {
     
     public convenience init(timeStyle: DateFormatter.Style) {
         self.init(dateStyle: .none, timeStyle: timeStyle)
+    }   
+}
+
+final public class UTCDateFormatter: DateFormatter {
+    override init() {
+        super.init()
+        self.timeZone = TimeZone(abbreviation: "UTC")
     }
     
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
